f56ffc9659952fb72cd4480d9bf510821869fadd
Joseph Babb <jbabb1@asu.edu>
2014-08-10 18:55:05 -0700

    First attempt to fix a new scanner bug.

--- src/bcplus/parser/detail/ScannerRawSource.cpp
+++ src/bcplus/parser/detail/ScannerRawSource.cpp
@@ -7,7 +7,7 @@ namespace detail {
 
 ScannerRawSource::ScannerRawSource(Configuration const* config)
 	: ScannerSource(ScannerSource::Type::RAW), _config(config), _cursor(NULL), _limit(NULL), 
-	  _token(NULL), _marker(NULL) {
+	  _token(NULL), _marker(NULL), _ctxmarker(NULL) {
 	// Intentionally left blank
 }
 
